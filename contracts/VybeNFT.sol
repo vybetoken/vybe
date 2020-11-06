bc2bfed2e54bb25ebb0d4ab77223d8e3b59a1f16
bighandsdev <63073747+bighandsdev@users.noreply.github.com>
2020-11-06 12:30:30 +0000

    Improvement(ERC721.sol) functionality for ERC721 (NFT) added

---
+++ contracts/VybeNFT.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.7.0;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract TierCard is ERC721 {
+
+    constructor() {
+        
+    }
+
+}
--- truffle-config.js
+++ truffle-config.js
@@ -1,30 +1,28 @@
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
 module.exports = {
-    networks: {
-        mainnet: {
-            provider: () => {return new HDWalletProvider(
-                "KEY",
-                "wss://mainnet.infura.io/ws/v3/"
-            )},
-            network_id: "*",
-            gas: 2500000,
-            gasPrice: 120000000000,
-            confirmations: 1,
-            timeoutBlocks: 500,
-            skipDryRun: true
-        }
+  networks: {
+    mainnet: {
+      provider: () => {
+        return new HDWalletProvider("KEY", "wss://mainnet.infura.io/ws/v3/");
+      },
+      network_id: "*",
+      gas: 2500000,
+      gasPrice: 120000000000,
+      confirmations: 1,
+      timeoutBlocks: 500,
+      skipDryRun: true,
     },
-    mocha: {},
-    compilers: {
-        solc: {
-            version: "0.7.0",
-            settings: {
-                optimizer: {
-                    enabled: true,
-                    runs: 250
-                }
-            }
-        }
-    }
+  },
+  mocha: {},
+  compilers: {
+    solc: {
+      settings: {
+        optimizer: {
+          enabled: true,
+          runs: 250,
+        },
+      },
+    },
+  },
 };
