efb1e2c0bc1614dbbf7446bc2e6f5fa012cdcece
bighandsdev <63073747+bighandsdev@users.noreply.github.com>
2020-11-15 12:26:49 +0000

    Testing script(VybeStaker.js) added further testing scripts for  inflation

--- test/VybeStake.js
+++ test/VybeStake.js
@@ -39,19 +39,15 @@ contract("Vybe test", async (accounts) => {
     assert(new BigNumber(0).isEqualTo(await stake.staked(accounts[0])));
     assert(INITIAL.isEqualTo(await VYBE.balanceOf.call(accounts[0])));
   });
-  it("Test Staking results for 2 years and claiming rewards every month", async () => {
+  it("Test Staking results for 1 years and claiming rewards every month", async () => {
     let VYBE = await VybeToken.deployed();
     let stake = await VybeStake.deployed();
 
-    await VYBE.approve(stake.address, INITIAL);
-    await stake.increaseStake(INITIAL);
+    await VYBE.approve(stake.address, 10000);
+    await stake.increaseStake(10000);
+    var balanceExpected = 10000;
 
-    for (var i = 60; i < 600; i = i + 31) {
-      console.log("Test for " + i / 30 + " number of months");
-      let supplyAtStart = ONE.multipliedBy(await VYBE.totalSupply());
-      let balanceAtStart = await VYBE.balanceOf.call(accounts[0]);
-      let fundAtStart = await VYBE.balanceOf.call(accounts[1]);
-
+    for (var i = 0; i < 365; i = i + 31) {
       await new Promise((resolve) => {
         web3.currentProvider.send(
           {
@@ -64,39 +60,91 @@ contract("Vybe test", async (accounts) => {
         );
       });
       await stake.claimRewards().then(function (result) {});
-
-      let mintagePiece = supplyAtStart
-        // get the % of the supply given the amount of passed months
-        .dividedBy(new BigNumber(25 + i * 5))
-        // broken down further for the 5% for the devfund
-        .dividedBy(new BigNumber(20));
+      var balanceAfter = await stake.staked.call(accounts[0]);
 
       // ensure the staking rewards were paid
       let expected = await calculateExpected();
+
       function calculateExpected() {
         let monthsStakingFor = i / 30;
         let rewardPerMonth = 0;
         let reward = 0;
         if (monthsStakingFor >= 6) {
-          rewardPerMonth = INITIAL.multipliedBy(0.0083);
+          rewardPerMonth = balanceExpected * 0.0083;
         } else if (monthsStakingFor >= 3) {
-          rewardPerMonth = INITIAL.multipliedBy(0.0067);
+          rewardPerMonth = balanceExpected * 0.0067;
         } else if (monthsStakingFor >= 1) {
-          rewardPerMonth = INITIAL.multipliedBy(0.0042);
+          rewardPerMonth = balanceExpected * 0.0042;
         }
-        reward = rewardPerMonth.multipliedBy(monthsStakingFor);
-        console.log(reward.toFixed().toString() + "expected");
-
+        reward = rewardPerMonth * monthsStakingFor;
         return reward;
       }
+      balanceExpected = balanceExpected + expected;
+      console.log("----------------------");
+      console.log(
+        "\x1b[33m%s\x1b[0m",
+        `Expected balance for ${(i / 30).toFixed()} months:`
+      );
+      console.log("\x1b[36m%s\x1b[0m", balanceExpected.toString());
+      console.log("\x1b[33m%s\x1b[0m", `Actual balance:`);
+      console.log(balanceAfter.toString());
+      console.log("----------------------");
+
       // allow a 1% variance due to division rounding
     }
   });
-  it("Test Claiming NFT", async () => {
+  it("Test Staking results for 2 year and only claiming the reward every 6 month", async () => {
     let VYBE = await VybeToken.deployed();
     let stake = await VybeStake.deployed();
-    await VYBE.approve(stake.address, INITIAL);
-    await stake.increaseStake(INITIAL);
-    await stake.claimNFT(accounts[0]);
+
+    await VYBE.approve(stake.address, 10000);
+    await stake.increaseStake(10000);
+    var balanceExpected = 10000;
+
+    for (var i = 212; i < 700; i = i + 212) {
+      await new Promise((resolve) => {
+        web3.currentProvider.send(
+          {
+            jsonrpc: "2.0",
+            method: "evm_increaseTime",
+            params: [DAY * i],
+            id: null,
+          },
+          resolve
+        );
+      });
+      await stake.claimRewards().then(function (result) {});
+      var balanceAfter = await stake.staked.call(accounts[0]);
+
+      // ensure the staking rewards were paid
+      let expected = await calculateExpected();
+
+      function calculateExpected() {
+        let monthsStakingFor = i / 30;
+        let rewardPerMonth = 0;
+        let reward = 0;
+        if (monthsStakingFor >= 6) {
+          rewardPerMonth = balanceExpected * 0.0083;
+        } else if (monthsStakingFor >= 3) {
+          rewardPerMonth = balanceExpected * 0.0067;
+        } else if (monthsStakingFor >= 1) {
+          rewardPerMonth = balanceExpected * 0.0042;
+        }
+        reward = rewardPerMonth * monthsStakingFor;
+        return reward;
+      }
+      balanceExpected = balanceExpected + expected;
+      console.log("----------------------");
+      console.log(
+        "\x1b[33m%s\x1b[0m",
+        `Expected balance for ${(i / 30).toFixed()} months:`
+      );
+      console.log("\x1b[36m%s\x1b[0m", balanceExpected.toString());
+      console.log("\x1b[33m%s\x1b[0m", `Actual balance:`);
+      console.log(balanceAfter.toString());
+      console.log("----------------------");
+
+      // allow a 1% variance due to division rounding
+    }
   });
 });
