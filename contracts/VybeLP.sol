58f121e5d71b2e652a456618813dbbd0e005d23b
bighandsdev <63073747+bighandsdev@users.noreply.github.com>
2020-12-01 18:41:49 +0000

    Improvement(VybeLP.sol) added basic structure for write functions for the contract and subsequent events

--- contracts/VybeLP.sol
+++ contracts/VybeLP.sol
@@ -4,14 +4,36 @@ pragma solidity ^0.6.0;
 import "./Ownable.sol"
 
 contract VybeLP is Ownable{
-    Uni private _uni;
-    Vybe private _vybe
+    Uni private _UNI;
+    Vybe private _VYBE
 
-  constructor(address uniswap) public Ownable(msg.sender){
+    event LiquidityAdded(uint256 EthAmount, uint256 VybeAmount, address Provider);
+    event LiquidityRemoved(uint256 EthAmount, uint256 VybeAmount, address Provider);
+    event RewardsClaimed(uint256 Reward, address Provider);
+
+  constructor(address uniswap, address vybetoken) public Ownable(msg.sender){
      // Contract for the Vybe pool on uniswap
-     _uni = Uni(uniswap);
+     _UNI = Uni(uniswap);
+     _VYBE = Vybe(vybetoken);
+  }
 
+  function addLiquidity(uint256 VybeAmount, uint256 EthAmount) external {
+      require(amount > 0);
+      
+      
+    emit LiquidityAdded(EthAmount, VybeAmount, msg.sender);
 
+  }
+
+  function removeLiqudity(uint256 amount) external {
+
+      emit LiquidityRemoved(EthAmount, VybeAmount, msg.sender)
+  }
+  function claimRewards() external {
+
+      uint256 rewards;
+
+      emit RewardsClaimed(rewards, msg.sender);
   }
    
 
