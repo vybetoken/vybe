5f6772208766097cc3627e533fab66c0e438f54c
bighandsdev <63073747+bighandsdev@users.noreply.github.com>
2020-12-01 21:24:05 +0000

    Improvement(VybeLP.sol) addLiquidity() now converts ETH to Weth and adds liquidity to uniswap, lastClaim and balance is then updated.

--- contracts/VybeLP.sol
+++ contracts/VybeLP.sol
@@ -5,7 +5,8 @@ import "./IOwnershipTransferrable.sol";
 
 contract VybeLP is Ownable{
     Uni private _UNI;
-    Vybe private _VYBE
+    Vybe private _VYBE;
+    Weth private _WETH;
 
     event LiquidityAdded(uint256 EthAmount, uint256 VybeAmount, address Provider);
     event LiquidityRemoved(uint256 EthAmount, uint256 VybeAmount, address Provider);
@@ -23,9 +24,12 @@ contract VybeLP is Ownable{
 
   function addLiquidity(uint256 EthAmount, uint256 VybeAmount) external {
       require(amount > 0);
-
-
-      
+      IWETH(WETH).deposit{value: EthAmount}()
+      uint256 maxTime = block.timestamp + 30;
+      require(IUniswapV2Router01(_UNI).addLiquidity(_WETH,_VYBE,ETHAmount,VybeAmount,ETHAmount,VybeAmount,msg.sender,maxTime));
+      _LPLastClaim[msg.sender] = block.timestamp;
+      // This is temp, really we need to check the exchange rate so find give them a fair value.
+      _LPbalances[msg.sender] = VybeAmount;
       
     emit LiquidityAdded(EthAmount, VybeAmount, msg.sender);
 
