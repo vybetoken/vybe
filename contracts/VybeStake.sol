62eba54fae64fb16e20ab3db012aac3983374921
bighandsdev <63073747+bighandsdev@users.noreply.github.com>
2020-12-15 08:25:38 +0000

    fix

--- contracts/VybeStake.sol
+++ contracts/VybeStake.sol
@@ -102,7 +102,7 @@ contract VybeStake is ReentrancyGuard, Ownable {
         return _lastClaim[staker];
     }
 
-    function rewardAvailable(address staker) external view returns (uint256) {
+    function rewardAvailable(address staker) external view returns (bool) {
         if (_lastClaim[staker].sub(block.timestamp) > 30 days) {
             return true;
         } else {
